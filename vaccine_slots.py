80d2c0ee059999a35fbd620fad596c2cf65a3fed
Suyash Saxena <suyash2896@gmail.com>
2021-05-10 01:40:43 +0530

    Add files via upload

---
+++ vaccine_slots.py
@@ -0,0 +1,62 @@
+import requests
+import datetime
+import http.client
+import json
+import pandas as pd
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import time
+
+pincodes = {
+    'basakvishwa95@gmail.com':['208001','208017','208002','208004','208011'],
+    'suyash2896@gmail.com':['201301','201014']
+}
+
+session_ids = set()
+
+def send_mail(found_info,receiver_id):
+    if len(found_info)>1:
+        df = pd.DataFrame(found_info,columns=['Centre','Pincode','Date','Age','Available Capacity','Vaccine'])
+        message = MIMEMultipart()
+        message['Subject'] = 'Found slots in your requested pincodes!'
+        message['From'] = "noreply.vaccines@gmail.com"
+        message['To'] = receiver_id
+        body_content = df.to_html()
+        message.attach(MIMEText(body_content, "html"))
+        msg_body = message.as_string()
+        server = SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(message['From'], 'covid@19')
+        server.sendmail(message['From'], message['To'], msg_body)
+        server.quit()
+
+def find_and_mail(pincodes,receiver_id):
+    found_info = []
+    for pincode in pincodes[receiver_id]:
+        params = {
+        'pincode':str(pincode),
+        'date':datetime.datetime.now().strftime('%d-%m-%y')
+        }
+        payload = ''
+        for key in params:
+            payload += key+'='+params[key]+'&'
+
+        conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
+        headers = {}
+        conn.request("GET", "/api/v2/appointment/sessions/public/calendarByPin?"+payload[:-1], '', headers)
+        res = conn.getresponse()
+        data = res.read()
+        data = json.loads(data.decode("utf-8"))
+        time.sleep(3)
+        for dt in data['centers']:
+            for session in dt['sessions']:
+                if session['available_capacity']>0:
+                    if session['session_id'] not in session_ids:
+                        found_info += [[dt['name'],dt['pincode'],session['date'],str(session['min_age_limit'])+'+',session['available_capacity'],session['vaccine']]]
+                        session_ids.add(session['session_id'])
+    send_mail(found_info,receiver_id)
+
+while(1):
+    for eid in pincodes:
+        find_and_mail(pincodes,eid)
